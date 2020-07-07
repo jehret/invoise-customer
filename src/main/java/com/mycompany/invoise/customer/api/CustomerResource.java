25ad0efd9c238faf2ddecde7e5712703f3072ac1
jp <jp.ehret@laposte.net>
2020-07-07 14:22:18 +1200

    resttemplate

--- src/main/java/com/mycompany/invoise/customer/api/CustomerResource.java
+++ src/main/java/com/mycompany/invoise/customer/api/CustomerResource.java
@@ -1,8 +1,6 @@
 package com.mycompany.invoise.customer.api;
 
 import com.mycompany.invoise.core.entity.customer.Customer;
-import com.mycompany.invoise.core.entity.invoice.Invoice;
-import com.mycompany.invoise.core.service.InvoiceServiceInterface;
 import com.mycompany.invoise.customer.repository.CustomerRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
