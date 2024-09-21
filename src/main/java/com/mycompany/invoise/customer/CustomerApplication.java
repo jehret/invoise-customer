f55042422b31313df537055e7c1b03f313496099
Edouard Leroy <leroyedouard@protonmail.com>
2024-09-21 18:20:54 +0200

    Migration V3

--- src/main/java/com/mycompany/invoise/customer/CustomerApplication.java
+++ src/main/java/com/mycompany/invoise/customer/CustomerApplication.java
@@ -3,11 +3,9 @@ package com.mycompany.invoise.customer;
 import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EntityScan("com.mycompany.invoise.core.entity.customer")
 public class CustomerApplication {
 
     public static void main(String[] args) {
@@ -16,9 +14,6 @@ public class CustomerApplication {
 
     @Bean
     public Hibernate6Module datatypeHibernateModule() {
-        Hibernate6Module module = new Hibernate6Module();
-        module.disable(Hibernate6Module.Feature.USE_TRANSIENT_ANNOTATION);
-        module.enable(Hibernate6Module.Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS);
-        return module;
+        return new Hibernate6Module();
     }
 }
--- src/main/java/com/mycompany/invoise/customer/repository/AddressRepositoryInterface.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.invoise.customer.repository;
-
-import com.mycompany.invoise.core.entity.customer.Address;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AddressRepositoryInterface extends CrudRepository<Address,Long> {
-
-}
